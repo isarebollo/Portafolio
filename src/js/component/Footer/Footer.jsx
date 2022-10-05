5e57e4f61e1eed2e80a5fa22570a8986862ff89b
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2022-10-05 11:20:30 +0200

    footer

--- src/js/component/Footer/Footer.jsx
+++ src/js/component/Footer/Footer.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import "../../component/Footer/Footer.css";
-import { BsLinkedin } from "react-icons/bs";
-import { GoMail } from "react-icons/go";
-import { FiGithub } from "react-icons/fi";
+
+import {AiOutlineCopyrightCircle} from "react-icons/ai";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <div className="contact">
-      <div className="iconsContact">
-        <a href="https://www.linkedin.com/in/isabelrebollo/">
-          <BsLinkedin />
-        </a>
-      </div>
-      <div className="iconsContact">
-        <GoMail />
-      </div>
-      <div className="iconsContact">
-        <a href="https://github.com/isarebollo">
-          <FiGithub />
-        </a>
-      </div>
+      <h6> <AiOutlineCopyrightCircle/> Copyright 2022 Isabel Rebollo</h6>
+      
     </div>
   </footer>
 );
