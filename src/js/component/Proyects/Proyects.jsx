00a205c7c9302175b0cce1071b859918175fa9f0
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2022-10-06 12:13:10 +0200

    responsive proyectos

--- src/js/component/Proyects/Proyects.jsx
+++ src/js/component/Proyects/Proyects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import playdating from "../../../img/Playdating.jpg";
+import playdating from "../../../img/PlayDating.jpg";
 // import Starwars from "../../../img/starwars.jpg";
 import "../../component/Proyects/Proyects.css";
 
@@ -10,7 +10,7 @@ export const Proyects = () => {
       <div className="section004">
         <h2 className="text-center title_h2_proyectos">Proyectos</h2>
 
-        <div className="container proyects col-12 mx-auto ">
+        <div className="container proyects">
           <div className="col-6">
             <div className="card">
               <img src={playdating} />
@@ -18,31 +18,17 @@ export const Proyects = () => {
                 <h5 className="card-title">PLAYDATING</h5>
                 <p className="card-text">WebApp de eventos infantiles</p>
 
-                <a href="https://playdating2.herokuapp.com">
+                <a href="https://play-dating-1.herokuapp.com">
                   <button className="btn btn-info">APP</button>
                 </a>
-                <a href="https://github.com/isarebollo/Child-WebApp-FinalProyect">
+                <a href="https://github.com/isarebollo/PlayDating">
                   <button className="btn btn-info">GitHub</button>
                 </a>
               </div>
             </div>
           </div>
-          {/* <div className="col-6">
-          <div className="card">
-            <img src={Starwars} />
-            <div className="card-body">
-              <h5 className="card-title">PLAYDATING</h5>
-              <p className="card-text">WebApp de eventos infantiles</p>
 
-              <a href="https://playdating2.herokuapp.com">
-                <button className="btn btn-info">APP</button>
-              </a>
-              <a href="https://github.com/isarebollo/Child-WebApp-FinalProyect">
-                <button className="btn btn-info">GitHub</button>
-              </a>
-            </div>
-          </div>
-        </div> */}
+
         </div>
       </div>
     </>
