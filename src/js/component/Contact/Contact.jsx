e6990f98947338b6e16be598b631872589d52c75
Isabel Rebollo <95927789+isarebollo@users.noreply.github.com>
2022-10-19 12:18:52 +0200

    css

--- src/js/component/Contact/Contact.jsx
+++ src/js/component/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 import emailjs from "@emailjs/browser";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
